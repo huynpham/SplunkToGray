8351d652356d32319891898d8ebd54eebe3dcd66
Huy Pham <huynpham@gmail.com>
2017-05-25 12:11:11 -0700

    Uses export instead of oneshot
    Uses export instead of oneshot function to extract logs from Splunk

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # SplunkToGray
-Convert splunk entries and send them to Graylog.
 
-You will need a couple of python lirbaries to get started:
+This is a different implementation of ImRaptor's script.
 
-1) Splunk Python SDK
+It uses Splunk's Python SDK export function instead of oneshot.  Export has the benefit of avoiding pagination issues.
+Recommend using nohup to run in background: nohup python SplukToGray.py > nohup_SplunkToGray.out 2>&1&
+
+
+
+Purpose:
+Export Splunk logs and send them to Graylog.
+
+You will need a couple of python libraries to get started:
+
+1) Splunk Python SDK (http://dev.splunk.com/python)
 
 2) Python Requests
 
-3) Python YAML
 
 
 They can be installed with pip
 
 * pip install splunk-sdk
 * pip install requests
-* pip install PyYAML
--- SplunkToGray.py
+++ SplunkToGray.py
@@ -1,69 +1,116 @@
-# Script will convert existing log files from Splunk to GrayLog
-
+# Script to export log files from Splunk to Graylog using Splunk SDK  
+# This script is written for linux systems, modify as necessary for your own OS.
+ 
 # Imports
 import os
 import time
-import yaml
 import splunklib.results as results
 import splunklib.client as client
 import requests as requests
+import json
+
+# Uncomment if you need to debug
+"""
+import logging
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+"""
 
 # Base configuration
 # Splunk search info
-HOST = "Splunk"  # Splunk server
+HOST = ""  # Splunk server
 PORT = 8089  # Splunk REST API port
-USERNAME = "admin"
-PASSWORD = "redacted"
-lineCount = 1000  # Number of results to process at a time
-searchModifier = "Host=DFSBack"  # Search modifier to target a subset of data. Set to * for everything
+USERNAME = ''
+PASSWORD = ''
 
 #  Graylog server info
-graylogPath = "http://graylog:12202/gelf"  # Address of a configured GELF HTTP Input on a Graylog server
+graylogPath = "http://graylog:12201/gelf"  # Address of a configured GELF HTTP Input on a Graylog server
 
-# Program configuration
-if os.name == 'nt':
-    path = "C:\\ProgramData\\SplunkConv\\"
+# Splunk export search parameters
+# For more details, go here: http://dev.splunk.com/view/python-sdk/SP-CAAAEE5
+
+# latest_time_filename is the name of the file containing the latest time, which should be in Unix time
+# If the file does not exist, the default will be the current time in Unix time
+# The file is used to keep track of our current progress in case the script exits for any reason
+latest_time_filename = "latest_time"
+
+# earliest_time should be in unix time
+earliest_time = ""
+
+# Define search parameters
+# "*" means to search everything
+# "| eval timestamp=_time" returns the log timestamp,_time, as unix time so we can easily export to graylog
+# _time is the timestamp in the log, while _indextime is the time that Splunk indexed the log. 
+searchquery_export = "search * | eval timestamp=_time"
+
+# open file to get the time where we left off
+if os.path.exists(latest_time_filename):
+    # if the file is empty, set the latest_time to the current time
+    if os.stat(latest_time_filename).st_size == 0:
+        with open(latest_time_filename, 'w') as f:
+            latest_time = str(time.time())
+            f.write(latest_time)
+    # read the file and set the latest_time
+    else:
+        with open(latest_time_filename, 'r') as f:
+            latest_time = f.readline()
+            latest_time = latest_time.strip('\n')
 else:
-    path = '/var/spool/SplunkConv/'
-timeMark = path+'time'
+    # if file does not exist, create file and set the latest_time to the current time
+    with open(latest_time_filename, 'w') as f:
+        latest_time = str(time.time())
+        f.write(latest_time)
 
-# Check for file and path
-endTime = ''
-if not os.path.isdir(path):
-    os.mkdir(path)
-elif os.path.isfile(timeMark):
-    with open(timeMark, 'r') as timeFile:
-        readBuff = yaml.load(timeFile)
-        for line in readBuff['search']:
-            if line['modifier'] == searchModifier:
-                endTime = line['time']
 
-# Deal with blank time
-if endTime is '':
-    endTime = str(int(time.time()))
 
-# Query Splunk and get results in JSON
+print "Latest Time is: {}".format(latest_time)
+print "Earliest Time is: {}".format(earliest_time)
+
 service = client.connect(host=HOST, port=PORT, username=USERNAME, password=PASSWORD)
-kwargs = {"latest_time": endTime, "time_format": "%s", "count": lineCount, "output_mode": "json"}
-searchQuery = "search "+searchModifier+"|head "+str(lineCount)
-oneshot = service.jobs.oneshot(searchQuery, **kwargs)
-reader = results.ResultsReader(oneshot)
 
-recTime = 0
-# Fill each record to a GELF log
-for res in reader:
-    gelf = {"version": "1.1",
-            "host": res["host"],
-            "short_message": res["_raw"],
-            "timestamp": res["_indextime"]}
-    recTime = int(res["_indextime"])
-    requests.post(graylogPath, json=gelf)
+# define the export parameters 
+kwargs_export = {"earliest_time": earliest_time,
+                 "latest_time": latest_time,
+                 "search_mode": "normal"}
 
-with open(timeMark, mode='rw') as timeFile:
-    buff = yaml.load(timeFile)
-    stack = {'search': []}
-    for line in buff['search']:
-        if line['modifier'] == searchModifier:
-            line['time'] = str(recTime-1)  # Move end time 1 second back to prevent retrieving the same logs again
-        stack['search'].append(line)
-    timeFile.write(yaml.dump(stack))
+
+exportsearch_results = service.jobs.export(searchquery_export, **kwargs_export)
+
+# Get the results and display them using the ResultsReader
+reader = results.ResultsReader(exportsearch_results)
+for result in reader:
+    if isinstance(result, dict):
+        # convert fields from Splunk into Graylog
+        # If your _raw logs are very large, you can set "full_message": result["_raw"], as well, but keep in mind Graylog requires the short_message field so you would be doubling your storage size
+        # http://docs.graylog.org/en/2.2/pages/gelf.html
+        gelf = {"version": "1.1",
+                "host": result["host"],
+                "_source_file": result["source"],
+                "short_message": result["_raw"],
+                "splunk_indextime": result["_indextime"],
+                "timestamp": result["timestamp"]}
+
+        latest_time = str(result["timestamp"])
+        with open(latest_time_filename, 'w') as f:
+            f.write(latest_time)
+
+        # Send to graylog
+        requests.post(graylogPath, json=gelf)
+
+    elif isinstance(result, results.Message):
+        # Diagnostic messages may be returned in the results
+        print "Message: %s" % result
+
